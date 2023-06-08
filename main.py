14062b7cce5a45430e9002c03f3de7d2b4d1f4bd
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-06-08 12:48:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ import pyautogui
 import time
 import keyboard
 
-# Set the target coordinates for clicking
-x = 500
-y = 500
+# Define the target coordinates for clicking
+click_positions = [(500, 500), (600, 600), (700, 700)]
 
 # Set the desired interval between clicks in seconds
 click_interval = 1
-
-# Set the number of clicks you want to perform
-num_clicks = 10
 
 # Set the key to stop the clicking process
 stop_key = "q"
 
 # Pause for a few seconds to allow you to position the mouse cursor
-print("Move your mouse to the desired clicking position...")
+print("Move your mouse to the desired clicking positions...")
 time.sleep(5)
 
 # Perform the clicks
 print("Press '{}' to stop the clicking process.".format(stop_key))
-for _ in range(num_clicks):
-    pyautogui.click(x, y)
-    time.sleep(click_interval)
-    
-    # Check if the stop key is pressed
-    if keyboard.is_pressed(stop_key):
-        print("Clicking process stopped.")
-        break
+try:
+    while True:
+        for position in click_positions:
+            pyautogui.click(position[0], position[1])
+            time.sleep(click_interval)
+
+            # Check if the stop key is pressed
+            if keyboard.is_pressed(stop_key):
+                print("Clicking process stopped.")
+                raise KeyboardInterrupt
+
+except KeyboardInterrupt:
+    print("Clicking process interrupted.")
 
 print("Clicking completed.")
